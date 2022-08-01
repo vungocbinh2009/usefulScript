a0d89346bce36bdb69234cfa7e3492199e1c33e7
binh <vungocbinh@protonmail.com>
2022-08-01 10:12:18 +0700

    Add automate_email script

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ Repo này bao gồm các script đơn giản, nhưng hữu ích cho công việc
 
 # Các script hiện có:
 - synthetic_data: Script dùng để tạo synthetic_data bằng Python, sử dụng thư viện pandas và mimesis
+- automate_email: Script dùng để gửi email tự động cho nhiều người (vd: sinh viên)
---
+++ automate_email/automate_email.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import yagmail
+
+df = pd.read_csv("danh_sach.csv")
+
+for index, row in df.iterrows():
+    # Sử dụng các thuộc tính trong row để chèn vào content và subject
+    yag = yagmail.SMTP('your-email', 'app-password')
+    subject = "subject"
+    contents = [
+        'This is the body, and here is just text http://somedomain/image.png',
+        'You can find an audio file attached.'
+    ]
+    yag.send('student-email', subject, contents)
